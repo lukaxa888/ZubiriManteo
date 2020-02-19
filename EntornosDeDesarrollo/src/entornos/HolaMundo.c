d994092ffa6821cc9d0b853337e680a924f90ca1
Xabi <Xabi@DESKTOP-AGEFCKG>
2020-02-19 22:40:55 +0100

    Subiendo los archivos necesarios para el trabajo de Entornos de Desarrollo.

---
+++ EntornosDeDesarrollo/src/entornos/HolaMundo.c
@@ -0,0 +1,13 @@
+/*
+  * Ejemplo HolaMundo 
+  * Imprime el mensaje "Hola, Mundo!" 
+*/
+
+#include <stdio.h>
+
+int main()
+{
+   printf( "Hola, Mundo desde C!" );
+
+   return 0;
+}
---
+++ EntornosDeDesarrollo/src/entornos/HolaMundo.java
@@ -0,0 +1,14 @@
+package entornos;
+class HolaMundo
+
+{
+
+ public static void main(String[] args)
+
+ {
+
+  System.out.println("Hola Mundo!");
+
+ }
+
+}
